fa6eebc201e463969f310db54b4f8fa73cc8c7b6
Mohit Soni <developer_mohitsoni@outlook.com>
2024-07-22 20:32:40 +0530

    Add @radix-ui/react-label and @radix-ui/react-slot as dependencies
    This commit adds the "@radix-ui/react-label" and "@radix-ui/react-slot" packages as dependencies in the package.json file. These packages are required for implementing label and slot components in the application. The versions added are "@radix-ui/react-label@2.1.0" and "@radix-ui/react-slot@1.1.0".

---
+++ Clash/frontend/src/app/(auth)/register/page.tsx
@@ -0,0 +1,69 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+import React from "react";
+
+const register = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-[550px] bg-white rounded-xl px-10 shadow-md py-5">
+        <h1 className="text-4xl font-extrabold bg-gradient-to-r from-pink-400 to-purple-500 text-center text-transparent bg-clip-text">
+          Clash
+        </h1>
+        <h1 className="text-3xl font-bold">Register</h1>
+        <p>Welcome to clash</p>
+        <form>
+          <div className="mt-4">
+            <Label htmlFor="name">Name</Label>
+            <Input
+              id="name"
+              type="text"
+              name="name"
+              placeholder="Enter your name..."
+            />
+          </div>
+          <div className="mt-4">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              name="email"
+              placeholder="Enter your email..."
+            />
+          </div>
+          <div className="mt-4">
+            <Label htmlFor="password">Password</Label>
+            <Input
+              id="password"
+              type="password"
+              name="password"
+              placeholder="Enter your password..."
+            />
+          </div>
+          <div className="mt-4">
+            <Label htmlFor="cpassword">Confirm Password</Label>
+            <Input
+              id="cpassword"
+              type="password"
+              name="confirm_password"
+              placeholder="Confirm your password..."
+            />
+          </div>
+          <div className="mt-4">
+            <Button className="w-full">Submit</Button>
+          </div>
+        </form>
+
+        <p className="text-center mt-2">
+          Already have an account ?{" "}
+          <strong>
+            <Link href="/login">Login</Link>
+          </strong>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default register;
