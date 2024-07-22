cdc8bccccf33e4f8147e3ac09cded5daaeca8a60
Mohit Soni <developer_mohitsoni@outlook.com>
2024-07-22 20:00:10 +0530

    Add HeroSection component to Home Page
    This commit adds the HeroSection component to the Home Page in order to improve the visual appeal and provide a hero section at the top of the page. The HeroSection component is imported from the "@/components/ui/base/HeroSection" module. The Home Page now displays the HeroSection component instead of the previous heading and button.

--- Clash/frontend/src/app/page.tsx
+++ Clash/frontend/src/app/page.tsx
@@ -1,11 +1,11 @@
+import HeroSection from "@/components/ui/base/HeroSection";
 import { Button } from "@/components/ui/button";
 import React from "react";
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-red-500">I am Home Page</h1>
-      <Button>Hello Next</Button>
+     <HeroSection/>
     </div>
   );
 };
