f24b88df4e392ac28e1b66c033773f947f99d3ad
Mohit Soni <developer_mohitsoni@outlook.com>
2024-07-22 18:11:56 +0530

    feat: Add Button component to Home Page
    This commit adds the Button component to the Home Page in order to enhance the user interface and provide additional functionality. The Button component is imported from the "@/components/ui/button" module. The Home Page now displays the text "I am Home Page" along with the Button component.

--- Clash/frontend/src/app/page.tsx
+++ Clash/frontend/src/app/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import React from "react";
 
 const App = () => {
   return (
-    <h1 className='text-red-500'>I am Home Page</h1>
-  )
-}
+    <div>
+      <h1 className="text-red-500">I am Home Page</h1>
+      <Button>Hello Next</Button>
+    </div>
+  );
+};
 
-export default App
+export default App;
