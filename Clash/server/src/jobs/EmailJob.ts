f182c00b7c4040663bcb933aa86f684d7c8b5e4b
Mohit Soni <developer_mohitsoni@outlook.com>
2024-07-25 20:17:54 +0530

    set the interface of EmailJobDataType and send what need to paas inside sendEmail()

--- Clash/server/src/jobs/EmailJob.ts
+++ Clash/server/src/jobs/EmailJob.ts
@@ -1,7 +1,14 @@
 import { Job, Queue, Worker } from "bullmq";
 import { defaultQueueOption, redisConnection } from "../config/queue.js";
+import { sendEmail } from "../config/mail.js";
 
 export const emailQueueName = "emailQueue";
+
+interface EmailJobDataType {
+  to: string;
+  subject: string;
+  body: string;
+}
 
 export const emailQueue = new Queue(emailQueueName, {
   connection: redisConnection,
@@ -13,9 +20,9 @@ export const emailQueue = new Queue(emailQueueName, {
 export const queueWorker = new Worker(
   emailQueueName,
   async (job: Job) => {
-    const data = job.data;
+    const data: EmailJobDataType = job.data;
 
-    console.log("The queue data is", data);
+    await sendEmail(data.to, data.subject, data.body);
   },
   {
     connection: redisConnection,
