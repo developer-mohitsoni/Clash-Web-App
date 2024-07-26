cebb0fee0286e36e773cf233b9663552f457ac32
Mohit Soni <developer_mohitsoni@outlook.com>
2024-07-26 22:44:05 +0530

    Perform "Encryption" the password using "bcrypt" package. And check whether the use already exist or not and Signup their Registration

--- Clash/server/src/routes/authRoutes.ts
+++ Clash/server/src/routes/authRoutes.ts
@@ -1,5 +1,9 @@
 import { Router, Request, Response } from "express";
 import { registerSchema } from "../validation/authValidation.js";
+import { ZodError } from "zod";
+import { formateError } from "../helper.js";
+import bcrypt from "bcrypt";
+import prisma from "../config/database.js";
 
 const router = Router();
 
@@ -10,9 +14,47 @@ router.post("/register", async (req: Request, res: Response) => {
     const body = req.body;
     const payload = await registerSchema.parse(body);
 
+    let user = await prisma.user.findUnique({
+      where: {
+        email: payload.email,
+      },
+    });
+    if (user) {
+      return res.status(422).json({
+        errors: {
+          email: "Email already taken. Please use another email",
+        },
+      });
+    }
+
+    //* Encrypt the password
+    const salt = await bcrypt.genSalt(10);
+    payload.password = await bcrypt.hash(payload.password, salt);
+
+    await prisma.user.create({
+      data: {
+        name: payload.name,
+        email: payload.email,
+        password: payload.password
+      }
+    });
+
+    return res.json({
+      message: "Account Created Successfully",
+    });
+
     return res.json(payload);
   } catch (error) {
-    return res.status(422).json(error);
+    if (error instanceof ZodError) {
+      const errors = formateError(error);
+      return res.status(422).json({
+        message: "Invalid Data",
+        errors,
+      });
+    }
+    return res.status(500).json({
+      message: "Something went wrong... Please try again",
+    });
   }
 });
 
