99dfe4ef2aefef9360830d23440f2a979c359109
Mohit Soni <developer_mohitsoni@outlook.com>
2024-07-26 22:16:46 +0530

    The commit adds a new route `/register` to handle user registration requests. It includes validation using the `zod` package to ensure the data sent in the request body is valid.

--- Clash/server/src/routes/authRoutes.ts
+++ Clash/server/src/routes/authRoutes.ts
@@ -1,11 +1,19 @@
 import { Router, Request, Response } from "express";
+import { registerSchema } from "../validation/authValidation.js";
 
 const router = Router();
 
 //* Register Route
 
-router.post("/",async(req:Request, res:Response)=>{
-    const body = req.body
-})
+router.post("/register", async (req: Request, res: Response) => {
+  try {
+    const body = req.body;
+    const payload = await registerSchema.parse(body);
+
+    return res.json(payload);
+  } catch (error) {
+    return res.status(422).json(error);
+  }
+});
 
 export default router;
