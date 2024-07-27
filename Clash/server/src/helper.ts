cd4ac883978fc912b014647f741ff03957c8ff33
Mohit Soni <developer_mohitsoni@outlook.com>
2024-07-27 22:18:25 +0530

    The commit adds a new helper function `renderEmailEjs` to the `helper.ts` file. This function takes a filename and payload as input and returns a Promise that resolves to a string. It uses the `ejs` package to render an email template file located in the `views/emails` directory. The rendered HTML is then returned as the result of the Promise.

--- Clash/server/src/helper.ts
+++ Clash/server/src/helper.ts
@@ -1,4 +1,7 @@
 import { ZodError } from "zod";
+import ejs from "ejs";
+import path from "path";
+import { fileURLToPath } from "url";
 
 export const formateError = (error: ZodError): any => {
   let errors: any = {};
@@ -6,4 +9,18 @@ export const formateError = (error: ZodError): any => {
     errors[issue.path?.[0]] = issue.message;
   });
   return errors;
+};
+
+export const renderEmailEjs = async (
+  filename: string,
+  payload: any
+): Promise<string> => {
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+  const html:string = await ejs.renderFile(
+    __dirname + `/views/emails/${filename}.ejs`,
+    payload
+  );
+
+  return html;
 };
