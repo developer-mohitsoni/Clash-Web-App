f86e66a5b0f34096a75b78579940ceab4c291d82
Mohit Soni <developer_mohitsoni@outlook.com>
2024-07-28 21:14:35 +0530

    Code update

--- Clash/frontend/src/actions/authActions.ts
+++ Clash/frontend/src/actions/authActions.ts
@@ -1,19 +1,32 @@
-"use server"
+"use server";
 
-import { REGISTER_URL } from "@/lib/apiEndPoints"
-import axios from "axios"
+import { REGISTER_URL } from "@/lib/apiEndPoints";
+import axios, { AxiosError } from "axios";
 
-export const registerAction = async(formData: FormData)=>{
-    console.log("The form data is: ", formData);
-    
-    // try{
-    //     await axios.post(REGISTER_URL,{
-    //         name: formData.get("name"),
-    //         email: formData.get("email"),
-    //         password: formData.get("password"),
-    //         confirm_password: formData.get("confirm_password"),
-    //     })
-    // }catch(error){
+export const registerAction = async (prevState: any, formData: FormData) => {
+  try {
+    const {data} = await axios.post(REGISTER_URL, formData);
 
-    // }
-}
+    return {
+        status: 200,
+        message: data?.message??"Account Created Successfully! Please check your email and verify your email.",
+        errors: {}
+    }
+  } catch (error) {
+
+    if(error instanceof AxiosError){
+        if(error.response?.status === 422){
+            return{
+                status: 422,
+                message: error.response?.data?.message,
+                errors: error.response?.data?.errors,
+            }
+        }
+    }
+    return {
+      status: 500   ,
+      message: "Something went wrong... Please try again!",
+      errors: {},
+    };
+  }
+};
